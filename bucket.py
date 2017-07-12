aa7a881fe3808185660745822b070d560ca0c1d4
Colin Wagaba <ckwagaba@gmail.com>
2017-07-12 05:47:25 +0000

    needed to switch branch

--- bucket.py
+++ bucket.py
@@ -1,1 +1,20 @@
 """ Module contains bucket attributes and methods """
+
+class Bucket(User):
+    
+    # values initialized when the object is instantiated
+    def __init__(self, bucket_name, bucket_progress):
+        self.bucket_name = bucket_name
+        self.bucket_progress = 0 # progress is initially at 0 %
+        
+    def create_bucket(self):
+        # method code here
+        
+    def view_bucket(self):
+        # method code here
+        
+    def update_bucket(self):
+        # method code here
+        
+    def delete_bucket(self):
+        # method code here
