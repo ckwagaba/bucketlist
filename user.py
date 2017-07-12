597b7eb518c9fc1d4ef429c6507bcc548d555956
Colin Wagaba <ckwagaba@gmail.com>
2017-07-12 05:47:39 +0000

    needed to switch branch

--- user.py
+++ user.py
@@ -1,1 +1,22 @@
 """ Module contains user attributes and methods """
+
+class User(object):
+    
+    # values initialized when the object is instantiated
+    def __init__(self, user_name, user_email, user_password):
+        self.user_name = user_name
+        self.user_email = user_email
+        self.user_password = user_password
+        
+    def create_user(self):
+        # validate data and create user
+        
+    def login_user(self, email, password):
+        # check provided credentials against stored values
+        if email == self.user_name and password == self.user_password:
+            # create session and login user
+        else:
+            # prompt user to make another login attempt
+        
+    def logout_user(self):
+        # unset user's session and redirect to login page
