c061be1c4f70bbc5bf7692bc435c1b62fa7bd0c3
Colin Wagaba <ckwagaba@gmail.com>
2017-07-12 11:30:47 +0000

    getting back

--- test_user.py
+++ test_user.py
@@ -1,3 +1,12 @@
 """ Test user user class functionality """
 
+# importing the test library
 import pytest
+
+# importing from the class files
+from user import User
+from bucket import Bucket
+from goal import Goal
+
+""" The different test cases """
+
