22b5a33ecdde3157349ce52bdebe224669a74c68
Colin Wagaba <ckwagaba@gmail.com>
2017-07-12 11:07:05 +0000

    added links to the views

--- app/views.py
+++ app/views.py
@@ -7,7 +7,23 @@ from app import app
 @app.route('/')
 def index():
     return render_template("index.html")
+    
+@app.route('/home')
+def home():
+    return render_template("index.html")
 
 @app.route('/signin')
-def about():
+def signin():
     return render_template("sign_in.html")
+    
+@app.route('/register')
+def signup():
+    return render_template("index.html")
+    
+@app.route('/buckets')
+def buckets():
+    return render_template("buckets.html")
+    
+@app.route('/goals')
+def goals():
+    return render_template("goals.html")
