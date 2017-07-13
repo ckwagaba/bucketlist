70078da49ba8ec2cce4879ba207f4bcd7a72234a
Colin Wagaba <ckwagaba@gmail.com>
2017-07-13 23:44:28 +0000

    structuring views file

--- app/views.py
+++ app/views.py
@@ -4,26 +4,35 @@ from flask import render_template
 
 from app import app
 
+# landing page - which happens to also be the registration page
 @app.route('/')
 def index():
     return render_template("index.html")
-    
-@app.route('/home')
-def home():
-    return render_template("index.html")
 
-@app.route('/signin')
-def signin():
-    return render_template("sign_in.html")
+""" we handle user registration """
+
+@app.route('/register')
+def signup_page():
+    return render_template("index.html")
     
 @app.route('/register')
-def signup():
+def create_user():
     return render_template("index.html")
     
+""" we handle user login and logout """
+
+@app.route('/signin')
+def signin_page():
+    return render_template("sign_in.html")
+    
+""" we handle bucket operations """
+    
 @app.route('/buckets')
-def buckets():
+def view_buckets():
     return render_template("buckets.html")
     
+""" we handle goal operations """
+    
 @app.route('/goals')
-def goals():
+def view_goals():
     return render_template("goals.html")
