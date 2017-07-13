52e9cb07bda8dea4b8a0dd41e3233edcf6b58a65
Colin Wagaba <ckwagaba@gmail.com>
2017-07-13 01:18:47 +0000

    will return

--- tests/test_user.py
+++ tests/test_user.py
@@ -10,3 +10,16 @@ from functionality.goals import *
 
 """ The different test cases """
 
+# some fake data
+fake_user = {'name': 'Colin', 'email': 'ckwagaba@gmail.com', 'password': 'pwd'}
+new_user = User(fake_user['name'], fake_user['email'], fake_user['password'])
+
+def test_new_user_creation():
+    assert isinstance(new_user, User)
+    
+    # test return of user info
+    assert new_user.create_user() == fake_user
+    assert new_user.user_name == 'Colin'
+    
+def test_non_empty_string_input():
+    pass
