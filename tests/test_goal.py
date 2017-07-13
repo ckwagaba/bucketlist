184c5fe59cd7d558b1afc46fb5d09f5b77e29416
Colin Wagaba <ckwagaba@gmail.com>
2017-07-13 13:44:03 +0000

    goal tests

--- tests/test_goal.py
+++ tests/test_goal.py
@@ -13,10 +13,14 @@ new_goal = Goal(fake_goal['description'], fake_goal['status'])
 
 def test_view_goal_info():
     goal_info = new_goal.view_info()
-    # assert goal_info[description] == 
+    assert goal_info == fake_goal
 
-def test_update_goal_info():
-    pass
-
-def test_delete_goal():
-    pass
+def test_update_goal_description():
+    new_goal_description = 'Join Andela tomorrow'
+    new_goal.update_description(new_goal_description)
+    assert new_goal.goal_description == new_goal_description
+    
+def test_update_goal_status():
+    new_status = 2
+    new_goal.update_status(new_status)
+    assert new_goal.goal_status == new_status
