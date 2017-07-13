76a7bbaa24d72287cf2592329ffa3e58e6c45775
Colin Wagaba <ckwagaba@gmail.com>
2017-07-13 08:29:48 +0000

    progressing with tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -10,16 +10,30 @@ from functionality.goals import *
 
 """ The different test cases """
 
-# some fake data
-fake_user = {'name': 'Colin', 'email': 'ckwagaba@gmail.com', 'password': 'pwd'}
+# creating instances with fake data
 new_user = User(fake_user['name'], fake_user['email'], fake_user['password'])
+user_bucket = new_user.create_bucket(fake_user_bucket['name'], fake_user_bucket['goals'], fake_user_bucket['progress'], fake_user_bucket['user'])
 
 def test_new_user_creation():
     assert isinstance(new_user, User)
     
-    # test return of user info
+def test_return_of_user_object():
     assert new_user.create_user() == fake_user
+    
+def test_user_name():
     assert new_user.user_name == 'Colin'
     
-def test_non_empty_string_input():
+def test_non_empty_name_input():
+    assert new_user.user_name != None
+
+def test_create_bucket():
+    assert user_bucket == fake_user_bucket
+    
+def test_non_empty_bucket_name():
+    assert user_bucket['name'] != None
+
+def test_login():
+    pass
+
+def test_logout():
     pass
