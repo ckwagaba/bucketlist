fa0f70480bc56bd9bbd8a4612167d23aee08aa41
Colin Wagaba <ckwagaba@gmail.com>
2017-07-13 13:01:25 +0000

    coming back

--- tests/test_goal.py
+++ tests/test_goal.py
@@ -7,10 +7,13 @@ from functionality.user import *
 from functionality.buckets import *
 from functionality.goals import *
 
+new_goal = Goal(fake_goal['description'], fake_goal['status'])
+
 """ The different test cases """
 
 def test_view_goal_info():
-    pass
+    goal_info = new_goal.view_info()
+    # assert goal_info[description] == 
 
 def test_update_goal_info():
     pass
