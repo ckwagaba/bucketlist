a0de47978e42b1366c494bf8284da7c5c99d9fc0
Colin Wagaba <ckwagaba@gmail.com>
2017-07-13 12:06:40 +0000

    almost done with the tests

--- tests/test_goal.py
+++ tests/test_goal.py
@@ -8,3 +8,12 @@ from functionality.buckets import *
 from functionality.goals import *
 
 """ The different test cases """
+
+def test_view_goal_info():
+    pass
+
+def test_update_goal_info():
+    pass
+
+def test_delete_goal():
+    pass
