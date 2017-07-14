60436509b0b4bd0cda8011f242829372e6d70464
Colin Wagaba <ckwagaba@gmail.com>
2017-07-14 06:50:24 +0000

    authentication works! yeah!

--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
 # import required modules
 
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, session
 from app import app
 from functionality.user import *
 from functionality.buckets import *
 from functionality.goals import *
 
 # making the user object available to all
-this_user = {}
+this_user = None
 
 """ landing page - which happens to also be the registration page """
 
@@ -23,26 +23,25 @@ def signup_page():
     
 @app.route('/register', methods = ['POST', 'GET'])
 def create_user():
+    global this_user # global user object to store credentials
     if request.method == 'POST': # proper form submission
         # get values from request
         name = request.form['user_name']
         email = request.form['user_email']
         password = request.form['user_password']
-        # create user with values
+        # update global user object with values
         this_user = User(name, email, password, [])
-        this_user_name = this_user.user_name
+        # then redirect to sign in
         return redirect('/signin')
-        #return redirect(url_for('view_buckets', user_name = this_user_name))
     else: # hot urls
-        
-        name = request.form['user_name']
-        email = request.form['user_email']
-        password = request.form['user_password']
-        
+        # get values from request
+        name = request.args.get('user_name')
+        email = request.args.get('user_email')
+        password = request.args.get('user_password')
+        # update global user object with values
         this_user = User(name, email, password, [])
-        this_user_name = this_user.user_name
+        # then redirect to sign in
         return redirect('/signin')
-        #return redirect(url_for('view_buckets', user_name = this_user_name))
     
 """ we handle user login and logout """
 
@@ -52,21 +51,33 @@ def signin_page():
     
 @app.route('/signin', methods = ['POST', 'GET'])
 def login():
+    global this_user
     if request.method == 'POST': # proper form submission
         # get values from request
         email = request.form['user_email']
         password = request.form['user_password']
-        # sign in user with values
-        this_user.login(email, password)
-        this_user_name = this_user.user_name
-        return redirect(url_for('view_buckets', user_name = this_user_name))
+        # Authenticate user
+        login_token = this_user.login(email, password)
+        if login_token == 1: # successful login
+            this_user_name = this_user.user_name
+            return redirect(url_for('view_buckets', user_name = this_user_name))
+        else: # unsuccessful login
+            return redirect('/signin')
     else: # hot urls
-        email = request.form['user_email']
-        password = request.form['user_password']
+        # get values from request
+        email = request.args.get('user_email')
+        password = request.args.get('user_password')
+        # Authenticate user
+        login_token = this_user.login(email, password)
+        if login_token == 1: # successful login
+            this_user_name = this_user.user_name
+            return redirect(url_for('view_buckets', user_name = this_user_name))
+        else: # unsuccessful login
+            return redirect('/signin')
         
-        this_user.login(email, password)
-        this_user_name = this_user.user_name
-        return redirect(url_for('view_buckets', user_name = this_user_name))
+@app.route('/logout')
+def logout():
+    return redirect('/signin')
     
 """ we handle bucket operations """
     
